d7041b1c71ed073006d86127205bcd69378ffd11
Viktor Butovchenko <vbutovchenko@mail.ru>
2021-03-07 22:06:22 +0700

    add settings

--- db.py
+++ db.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgres://mnqwfzlh:Dvthtq9BU6RjsvfO-R7m9PVSnxxQWvSk@hattie.db.elephantsql.com:5432/mnqwfzlh')
+import settings
+
+engine = create_engine(db_connect)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
