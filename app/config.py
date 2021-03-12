9fc6901fd5deaf78a32997b0261c3780ed126e83
Viktor Butovchenko <vbutovchenko@mail.ru>
2021-03-12 13:40:17 +0700

    add models

--- app/config.py
+++
@@ -1,1 +0,0 @@
-SQLALCHEMY_DATABASE_URI = "postgres://mnqwfzlh:Dvthtq9BU6RjsvfO-R7m9PVSnxxQWvSk@hattie.db.elephantsql.com:5432/mnqwfzlh"
--- app/models.py
+++ app/models.py
@@ -1,46 +1,95 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+class RecordType(enum.Enum):
+    CONFIRMED = "Confimed"
+    UNCONFIRMED = "Unconfimed"
+
+class CorrectionType(enum.Enum):
+    UNWORK = "Unwork"
+    WORK = "Work"
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     firstname = db.Column(db.String(50), nullable = False)
     lastname = db.Column(db.String(50), nullable = True)
     birthday = db.Column(db.Date, nullable = True)
-    telephone = db.Column(db.String(15), nullable = False, unique = True)
-    email = db.Column(db.String(150), nullable = True, unique=True)
-    rool_id = db.Column(db.Integer, db.ForeignKey('rools.id'), nullable = False)
-    records = db.relationship('Record', 
-        backref =  db.backref('user', lazy = 'joined'), lazy = 'dynamic')
+    phone = db.Column(db.String(15), nullable = False, unique = True)
+    email = db.Column(db.String(150), nullable = True, unique = True)
+    records = db.relationship(
+        'Record', 
+        backref =  db.backref('client', lazy = 'joined'),
+        lazy = 'dynamic'
+        )
 
     def __repr__(self):
-        return f'<User {self.firstname} {self.lastname} {self.telephone}>'
+        return f'<Client {self.firstname} {self.lastname} {self.telephone}>'
 
-class Rool(db.Model):
-    __tablename__ = 'rools'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable = False)
+class Worker(db.Model):
+    __tablename__ = 'workers'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname = db.Column(db.String(50), nullable = False)
+    lastname = db.Column(db.String(50), nullable = True)
+    birthday = db.Column(db.Date, nullable = True)
+    phone = db.Column(db.String(15), nullable = False, unique = True)
+    email = db.Column(db.String(150), nullable = True, unique = True)
+    records = db.relationship(
+        'Record', 
+        backref =  db.backref('worker', lazy = 'joined'),
+        lazy = 'dynamic'
+        )
+    corrections = db.relationship(
+        'Correction', 
+        backref =  db.backref('worker', lazy = 'joined'),
+        lazy = 'dynamic'
+        )
+    shedules = db.relationship(
+        'Shedule', 
+        backref =  db.backref('worker', lazy = 'joined'),
+        lazy = 'dynamic'
+        )
 
     def __repr__(self):
-        return f'<Rool {self.name}>'
-     
+        return f'<Worker {self.firstname} {self.lastname} {self.telephone}>'
+
+class Administrator(db.Model):
+    __tablename__ = 'administrators'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname = db.Column(db.String(50), nullable = False)
+    lastname = db.Column(db.String(50), nullable = True)
+    birthday = db.Column(db.Date, nullable = True)
+    phone = db.Column(db.String(15), nullable = False, unique = True)
+    email = db.Column(db.String(150), nullable = True, unique = True)
+    
+    def __repr__(self):
+        return f'<Administrator {self.firstname} {self.lastname} {self.telephone}>'
+
 class Record(db.Model):
     __tablename__ = 'records'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable = False)
+    worker_id = db.Column(db.Integer, db.ForeignKey('workers.id'), nullable = False)
     text = db.Column(db.Text, nullable = True)
-    publish_date = db.Column(db.DateTime, nullable = False) 
-    record_date = db.Column(db.DateTime, nullable = False)
-    record_type_id = db.Column(db.Integer, db.ForeignKey('record_types.id'), nullable = False)
+    published = db.Column(db.DateTime, nullable = False) 
+    start_time = db.Column(db.TIMESTAMP, nullable = False)
+    duration = db.Column(db.Integer, nullable = False)
+    status = db.Column(db.Enum(RecordType))
 
     def __repr__(self):
         return f'<Record {self.user_id} {self.text}>'
 
-class Record_type(db.Model):
-    __tablename__ = 'record_types'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable = False)
-    
-    def __repr__(self):
-        return f'<Record_type {self.name}>'
+class Correction(db.Model):
+    __tablename__ = 'corrections'
+    worker_id = db.Column(db.Integer, db.ForeignKey('workers.id'), primary_key=True, nullable = False)
+    start_time = db.Column(db.TIMESTAMP, primary_key=True, nullable = False)
+    status = db.Column(db.Enum(CorrectionType))
+    duration = db.Column(db.Integer, nullable = False)
+
+class Shedule(db.Model):
+    __tablename__ = 'shedules'
+    worker_id = db.Column(db.Integer, db.ForeignKey('workers.id'), primary_key=True, nullable = False)
+    start_time = db.Column(db.Integer, primary_key=True, nullable = False)
+    duration = db.Column(db.Integer, nullable = False)
