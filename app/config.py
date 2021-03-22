a8c8ec5dd28a4ae69470201a63982f6091bc162d
Viktor Butovchenko <vbutovchenko@mail.ru>
2021-03-22 22:59:36 +0700

    Hide secret key

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-SECRET_KEY = "dmwLMAWDkK(DJ@djiasd8@nddnakwdn_dawd9ji"
+SECRET_KEY = os.getenv('SECRET_KEY')
